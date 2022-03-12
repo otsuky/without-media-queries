e949a312ed102e708c1c1cd81ed1054d71c2552c
Y.Otsuki <vloxolv.otky.vlowolv@gmail.com>
2022-03-12 23:24:47 +0900

    first commit

---
+++ .storybook/main.js
@@ -0,0 +1,5 @@
+module.exports = {
+  stories: ["../src/**/*.stories.@(jsx|tsx)"],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-interactions"],
+  framework: "@storybook/react",
+};
---
+++ src/components/stories/Button.stories.tsx
@@ -0,0 +1,38 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Button } from "./Button";
+
+const meta: ComponentMeta<typeof Button> = {
+  title: "Example/Button",
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+};
+export default meta;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
+  label: "Button",
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: "Button",
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: "large",
+  label: "Button",
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: "small",
+  label: "Button",
+};
