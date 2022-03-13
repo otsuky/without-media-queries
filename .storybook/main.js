e1f0ccc133bc0e5d67a6c81bc4637db8c6ab7645
Y.Otsuki <vloxolv.otky.vlowolv@gmail.com>
2022-03-13 22:43:33 +0900

    css modules 対応

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,30 @@
+const path = require("path");
+
 module.exports = {
   stories: ["../src/**/*.stories.@(jsx|tsx)"],
   addons: ["@storybook/addon-essentials"],
   framework: "@storybook/react",
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: {
+            modules: {
+              auto: true,
+            },
+          },
+        },
+        "sass-loader",
+      ],
+      include: path.resolve(__dirname, "../"),
+    });
+    return config;
+  },
+
+  core: {
+    builder: "webpack5",
+  },
 };
