584f80621dbe87153292936c01abac7df9682b02
yukako-ootsuki <yukako_ootsuki@waku-2.com>
2022-03-15 10:11:10 +0900

    eslint 改善

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import ReactDOM from "react-dom";
-import React from "react";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 import "./styles/reset.css";
 import "./styles/base.css";
 import { App } from "./App";
